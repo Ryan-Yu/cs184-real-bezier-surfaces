a5ff7a92b7ab10bb2034a8423f55f3c81f9707e1
Ryan Yu <ryan.cao.you@gmail.com>
2015-04-11 14:12:02 -0700

    Add accessors for BezierPatch class

--- BezierPatch.h
+++ BezierPatch.h
@@ -28,6 +28,20 @@ class BezierPatch {
 	void addCurve(std::vector<Eigen::Vector3f> curve) {
 		listOfCurves.push_back(curve);
 	}
+
+	void addTriangle(Eigen::Vector3f vertex1, Eigen::Vector3f vertex2, Eigen::Vector3f vertex3) {
+		listOfTriangles.push_back(Triangle(vertex1, vertex2, vertex3));
+	}
+
+	void addTriangle(float ax, float ay, float az,
+			float bx, float by, float bz,
+			float cx, float cy, float cz) {
+		listOfTriangles.push_back(Triangle(ax, ay, az, bx, by, bz, cx, cy, cz));
+	}
+
+	void addDifferentialGeometry(Eigen::Vector3f position, Eigen::Vector3f normal, Eigen::Vector2f uvValues) {
+		listOfDifferentialGeometries.push_back(DifferentialGeometry(position, normal, uvValues));
+	}
 };
 
 #endif /* BEZIERPATCH_H_ */
