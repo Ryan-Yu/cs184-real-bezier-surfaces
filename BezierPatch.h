bcdc3d2aca5f1b9def61fd787ef7f7e43307e95a
Ryan Yu <ryan.cao.you@gmail.com>
2015-04-12 13:49:31 -0700

    Add generation of triangles from points in BezierPatch uniform tesselation

--- BezierPatch.h
+++ BezierPatch.h
@@ -39,6 +39,15 @@ class BezierPatch {
 
 
 	//****************************************************
+	// TODO: Method that generates a DifferentialGeometry object that represents
+	// the result of evaluating 'this' BezierPatch at (u, v)
+	//***************************************************
+	DifferentialGeometry evaluateDifferentialGeometry(float u, float v) {
+
+	}
+
+
+	//****************************************************
 	// TODO: Method that populates each BezierPatch's list of DifferentialGeometries
 	// and list of Triangles, based on adaptive subdivision
 	//***************************************************
@@ -52,7 +61,66 @@ class BezierPatch {
 	// and list of Triangles, based on uniform subdivision
 	//***************************************************
 	void performUniformSubdivision(float stepSize) {
+		int numberOfSteps = 1.0 / stepSize;
+		for (int u = 0; u < numberOfSteps; u++) {
+			for (int v = 0; v < numberOfSteps; v++) {
+				// Evaluate the differential geometry at (u * stepSize, v * stepSize(
+				// For instance, if stepSize = 0.1, then we would evaluate at (0, 0), (0, 0.1), (0, 0.2), etc
+				listOfDifferentialGeometries.push_back(evaluateDifferentialGeometry(u * stepSize, v * stepSize));
+			}
+		}
 
+		// NOTE: Code confirmed as working (tested)
+		// Populate the list of Triangles, based on the list of points in listOfDifferentialGeometries
+		// By ordering above, the DifferentialGeometries are ordered with the following numbering:
+		//
+		// 1 6  11 16 21
+		// 2 7  12 17 22
+		// 3 8  13 18 23
+		// 4 9  14 19 24
+		// 5 10 15 20 25
+		//
+		// where numberOfSteps = 5
+		//
+		// NOTE: By construction, our grid will always be n x n (i.e. a square)
+		//
+		// Basic Algorithm: for every 4 points in a rectangle, like below,
+		//
+		// 1 2
+		// 3 4
+		//
+		// pair the points up in two triangles like so:
+		//
+		// 1--2
+		// |/ |
+		// 3--4
+		//
+		// so that 1-2-3 form a triangle (tri-1) and 2-3-4 form a triangle (tri-2)
+
+		// Iterate through all of our differential geometries, but do NOT touch the right-most column and the bottom-most row
+		for (int u = 0; u < (numberOfSteps - 1); u++) {
+			for (int v = 0; v < (numberOfSteps - 1); v++) {
+				// (u, v) represents the index in the above grid that we're triangulating
+				// This index represents the TOP LEFT corner of the 4-point rectangle that is described above
+
+				// Index of listOfDifferentialGeometries that corresponds with position (u, v)
+				int differentialGeometrixIndex = (u * numberOfSteps) + v;
+
+				// Construct tri-1
+				listOfTriangles.push_back(Triangle(
+						listOfDifferentialGeometries[differentialGeometrixIndex], // top left
+						listOfDifferentialGeometries[differentialGeometrixIndex + numberOfSteps], // move one unit right
+						listOfDifferentialGeometries[differentialGeometrixIndex + 1])); // move one unit down from top left
+
+				// Construct tri-2
+				listOfTriangles.push_back(Triangle(
+						listOfDifferentialGeometries[differentialGeometrixIndex + numberOfSteps], // top right
+						listOfDifferentialGeometries[differentialGeometrixIndex + 1], // bottom left
+						listOfDifferentialGeometries[differentialGeometrixIndex + numberOfSteps + 1])); // bottom right
+			}
+		}
+
+		// We should have (numberOfSteps - 1) * (numberOfSteps - 1) * 2 triangles
 	}
 };
 
